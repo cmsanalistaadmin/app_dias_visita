a7b0b36211d1802d41b12e5f9a96e0a5b8105c1b
Patricio Venero <veneromedinap@gmail.com>
2025-03-26 16:41:11 -0500

    prueba 1

---
+++ app/src/main/java/com/example/visita_frias/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.visita_frias
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.visita_frias.ui.theme.VISITA_FRIASTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            VISITA_FRIASTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "HOLa mundo espero que esten bien probar 3 121321321",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    VISITA_FRIASTheme {
+        Greeting("Android")
+    }
+}
